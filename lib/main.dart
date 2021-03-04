d4881a599d3df2b29fed9326711e49da47fe2633
ayusuke7 <alexhcb.7392@gmail.com>
2021-03-04 13:50:12 -0300

    Video Aula - Layout do Aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
